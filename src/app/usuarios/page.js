2d1ec43e4928ca07d53922b140f972d327ab796a
John Vicent <jmgvicent@gmail.com>
2026-02-03 16:15:12 +0100

    feat: Add Usuarios link to navigation and create Usuarios page component

---
+++ src/app/usuarios/page.js
@@ -0,0 +1,7 @@
+export default function UsuariosPage() {
+  return (
+    <div>
+      <h2>Estos son nuestros usuarios</h2>
+    </div>
+  );
+}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -27,6 +27,12 @@ export default function Navigation() {
               >
                 Galería
               </Link>
+              <Link
+                href="/usuarios"
+                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+              >
+                Usuarios
+              </Link>
             </div>
           </div>
           <div className="flex items-center">
